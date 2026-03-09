dd0462f3a6f3ada30baf061d1fcc02ea7191d9ea
Timileyin <tolalere@axxess.com>
2026-03-09 12:02:19 +0100

    Second Commit

--- FinanceApp/Models/Expense.cs
+++ FinanceApp/Models/Expense.cs
@@ -3,8 +3,8 @@
     public class Expense
     {
         public int Id { get; set; }
-        public string Description { get; set;  }
-
-
+        public string Description { get; set;}
+        public string Amount { get; set; }
+        public string Category { get; set; }
     }
 }
